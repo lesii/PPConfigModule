cfc7d3eaa21da214d457072ef195872317e6db6b
le_sii <le_sii@hotmail.com>
2022-08-05 13:51:41 +0800

    Add New ProjectSetting

---
+++ PProjectSetting/TestSetting.cs
@@ -0,0 +1,11 @@
+using PPExtensionModule;
+
+
+public class TestSetting : PPSettingBase
+{
+    public string testVal; //---Delete_This_Comment_To_Prevent_Change!
+    public int testIVal; //---Delete_This_Comment_To_Prevent_Change!
+    public float testFVal; //---Delete_This_Comment_To_Prevent_Change!
+    public bool testbBal; //---Delete_This_Comment_To_Prevent_Change!
+//#
+}
